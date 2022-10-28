556330cfcdab0fc133a430aac014ff6f1c02632a
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2022-10-28 17:56:22 +0200

    First version

--- src/app.py
+++ src/app.py
@@ -3,3 +3,166 @@ from discord.ext import commands, tasks
 import os
 from dotenv import load_dotenv
 import youtube_dl
+import asyncio
+
+load_dotenv()
+
+DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
+
+
+youtube_dl.utils.bug_reports_message = lambda: ""
+
+ytdl_format_options = {
+    "format": "bestaudio/best",
+    "restrictfilenames": True,
+    "noplaylist": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "quiet": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {"options": "-vn"}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get("title")
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(
+            None, lambda: ytdl.extract_info(url, download=not stream)
+        )
+        if "entries" in data:
+            # take first item from a playlist
+            data = data["entries"][0]
+        filename = data["title"] if stream else ytdl.prepare_filename(data)
+        return filename
+
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
+        """
+        Joins a voice channel
+        """
+        if ctx.author.voice and ctx.author.voice.channel:
+            channel = ctx.message.author.voice.channel
+        else:
+            await ctx.send(
+                f"{ctx.message.author.name} is not connected to a voice channel"
+            )
+            return None
+        await channel.connect()
+
+    @commands.command()
+    async def leave(ctx):
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_connected():
+            await voice_client.disconnect()
+        else:
+            await ctx.send("Vidify is not connected to a voice channel.")
+
+    @commands.command()
+    async def play(ctx, url):
+        """Streams from a url"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(
+                player, after=lambda e: print(f"Player error: {e}") if e else None
+            )
+
+        await ctx.send(f"Now playing: {player.title}")
+
+    @commands.command()
+    async def pause(ctx):
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_playing():
+            voice_client.pause()
+        else:
+            await ctx.send("Vidify is not playing")
+
+    @commands.command()
+    async def resume(ctx):
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_paused():
+            voice_client.resume()
+        else:
+            await ctx.send("Vidify is not paused")
+
+    @commands.command()
+    async def stop(ctx):
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_playing():
+            voice_client.stop()
+        else:
+            await ctx.send("Vidify is not playing")
+
+    @commands.command()
+    async def where_am_i(ctx):
+        owner = str(ctx.guild.owner)
+        region = str(ctx.guild.region)
+        guild_id = str(ctx.guild.id)
+        memberCount = str(ctx.guild.member_count)
+        icon = str(ctx.guild.icon_url)
+        desc = ctx.guild.description
+
+        embed = discord.Embed(
+            title=ctx.guild.name + " Server Information",
+            description=desc,
+            color=discord.Color.blue(),
+        )
+        embed.set_thumbnail(url=icon)
+        embed.add_field(name="Owner", value=owner, inline=True)
+        embed.add_field(name="Server ID", value=guild_id, inline=True)
+        embed.add_field(name="Region", value=region, inline=True)
+        embed.add_field(name="Member Count", value=memberCount, inline=True)
+
+        await ctx.send(embed=embed)
+
+        members = []
+        async for member in ctx.guild.fetch_members(limit=150):
+            await ctx.send(
+                "Name : {}\t Status : {}\n Joined at {}".format(
+                    member.display_name, str(member.status), str(member.joined_at)
+                )
+            )
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or("!"),
+    description="Relatively simple music bot example",
+    intents=intents,
+)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
+
+async def main():
+    async with bot:
+        await bot.add_cog(Music(bot))
+        await bot.start(DISCORD_TOKEN)
+
+
+asyncio.run(main())
